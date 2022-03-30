bb7b90a96f4b4fe31b8cb3af61600fc4a0dcdc43
FlapShatner <94399441+FlapShatner@users.noreply.github.com>
2022-03-29 21:03:36 -0500

    notes fetched and displayed in ticket

--- frontend/src/pages/Ticket.jsx
+++ frontend/src/pages/Ticket.jsx
@@ -1,13 +1,17 @@
 import { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { getTicket, reset, closeTicket } from '../features/tickets/ticketSlice'
+import { getNotes, reset as notesReset } from '../features/notes/noteSlice'
 import { useParams, useNavigate } from 'react-router-dom'
 import { toast } from 'react-toastify'
+import NoteItem from '../components/NoteItem'
 import BackButton from '../components/BackButton'
 import Spinner from '../components/Spinner'
 
 function Ticket() {
   const { ticket, isLoading, isSuccess, isError, message } = useSelector((state) => state.tickets)
+
+  const { notes, isLoading: notesIsLoading } = useSelector((state) => state.notes)
 
   const params = useParams()
   const dispatch = useDispatch()
@@ -20,6 +24,7 @@ function Ticket() {
     }
 
     dispatch(getTicket(ticketId))
+    dispatch(getNotes(ticketId))
     // eslint-disable-next-line
   }, [isError, message, ticketId])
 
@@ -30,7 +35,7 @@ function Ticket() {
     navigate('/tickets')
   }
 
-  if (isLoading) {
+  if (isLoading || notesIsLoading) {
     return <Spinner />
   }
 
@@ -58,7 +63,12 @@ function Ticket() {
           <h3>Description of issue</h3>
           <p>{ticket.description}</p>
         </div>
+        <h2>Notes</h2>
       </header>
+
+      {notes.map((note) => (
+        <NoteItem key={note._id} note={note} />
+      ))}
 
       {ticket.status !== 'closed' && (
         <button className='btn btn-block btn-danger' onClick={onTicketClose}>
