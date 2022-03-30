4cc31845630f69d3affdfdad9cd0f4626d30e9c9
FlapShatner <94399441+FlapShatner@users.noreply.github.com>
2022-03-29 19:02:43 -0500

    added single ticket view

--- frontend/src/features/tickets/ticketSlice.js
+++ frontend/src/features/tickets/ticketSlice.js
@@ -10,7 +10,7 @@ const initialState = {
   message: '',
 }
 
-// Create user tickets
+// Get user tickets
 export const getTickets = createAsyncThunk('tickets/getAll', async (_, thunkAPI) => {
   try {
     const token = thunkAPI.getState().auth.user.token
@@ -28,6 +28,19 @@ export const createTicket = createAsyncThunk('tickets/create', async (ticketData
   try {
     const token = thunkAPI.getState().auth.user.token
     return await ticketService.createTicket(ticketData, token)
+  } catch (error) {
+    const message =
+      (error.response && error.response.data && error.response.data.message) || error.message || error.toString()
+
+    return thunkAPI.rejectWithValue(message)
+  }
+})
+
+// Get user ticket
+export const getTicket = createAsyncThunk('tickets/get', async (ticketId, thunkAPI) => {
+  try {
+    const token = thunkAPI.getState().auth.user.token
+    return await ticketService.getTicket(ticketId, token)
   } catch (error) {
     const message =
       (error.response && error.response.data && error.response.data.message) || error.message || error.toString()
@@ -65,6 +78,19 @@ export const ticketSlice = createSlice({
         state.tickets = action.payload
       })
       .addCase(getTickets.rejected, (state, action) => {
+        state.isLoading = false
+        state.isError = true
+        state.message = action.payload
+      })
+      .addCase(getTicket.pending, (state) => {
+        state.isLoading = true
+      })
+      .addCase(getTicket.fulfilled, (state, action) => {
+        state.isLoading = false
+        state.isSuccess = true
+        state.ticket = action.payload
+      })
+      .addCase(getTicket.rejected, (state, action) => {
         state.isLoading = false
         state.isError = true
         state.message = action.payload
